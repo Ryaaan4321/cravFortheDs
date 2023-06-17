16edf7a0b36aecb4d947901ee0614faf18f4c58b
aryan <91269171+Ryaaan4321@users.noreply.github.com>
2023-06-17 17:05:03 +0530

    singlenonduplicate.java

--- singlenonduplicate.java
+++ singlenonduplicate.java
@@ -23,6 +23,35 @@ public class singleleminarr {
         }
         return - 1;
     }
+    // bcs in the last appproach we are running t two conditions inside the if conditiond 
+    // nd there are some chances when it didnt enter into it so for tht condition this 2nd method will run smoothly like  a makhaan..!
+    
+    public int singleNonDuplicate(int[] nums) {
+        int n = nums.length;
+        int low = 0, high = n - 1;
+        
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+                            
+            if (mid % 2 == 0) {
+                if (nums[mid] == nums[mid + 1]) {
+                    low = mid + 2;
+                } else {
+                    high = mid;
+                }
+            } else {
+                if (nums[mid] == nums[mid - 1]) {
+                    low = mid + 1;
+                } else {
+                    high = mid;
+                }
+            }
+        }
+        
+        return nums[low];
+    }
+
+
      public static void main(String[]args){
         int nums[] = {4 , 4 , 6   , 6 , 1 , 1 , 3};
         int elemfound = singlenonduplicate(nums);
