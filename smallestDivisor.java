f4746bab2dbdfa6b79af603a44c61dff24ea770b
aryan <91269171+Ryaaan4321@users.noreply.github.com>
2023-06-23 22:18:40 +0530

    Add files via upload

---
+++ smallestDivisor.java
@@ -0,0 +1,37 @@
+package striver.binarysearch;
+
+import java.util.Arrays;
+
+public class smallestDivisor {
+    
+    public static int sumByd(int[] arr, int div) {
+        int sum = 0;
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            sum += Math.ceil((double) arr[i] / div);
+        }
+        return sum;
+    }
+    
+    public static int smallestDiv(int[] arr, int limit) {
+        int low = 1;
+        int high = Arrays.stream(arr).max().getAsInt(); // Calculate the maximum value in the array
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (sumByd(arr, mid) <= limit) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+    
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 6, 8, 10};
+        int limit = 10;
+        int result = smallestDiv(arr, limit);
+        System.out.println("Smallest divisor: " + result);
+    }
+}
